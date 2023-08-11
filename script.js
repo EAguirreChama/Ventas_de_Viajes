9e2ea30becf2fb0fb82bc70bb0e2894723f16ed5
EAguirre <aguirreernesto25@gmail.com>
2023-08-11 20:08:10 -0300

    Commit 1

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+import{ barcelona, roma, paris, londres } from './ciudades.js'
+
+// Obtener los elementos del DOM
+let enlaces = document.querySelectorAll('a');
+let tituloElemento = document.getElementById('titulo')
+let subTituloElemento = document.getElementById('subtitulo')
+let parrafoElemento = document.getElementById('parrafo')
 
